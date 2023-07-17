2fd217c35ced28547f3e4e7652ebd7609735d04a
KHJcode <to@khjcode.com>
2023-07-17 09:33:30 +0900

    feat: Show message when search result is blank

--- src/search/topics.ts
+++ src/search/topics.ts
@@ -9,14 +9,20 @@ export async function loadTopics(element: HTMLDivElement, keyword: string, histo
     });
     if (response.status === 200) {
         const result = response.data;
-        for (const topic of result) {
-            const topicItemTag = document.createElement('div');
-            topicItemTag.setAttribute('class', 'topic_item');
-            const topicTitleTag = document.createElement('a');
-            topicTitleTag.setAttribute('href', `detail.html?id=${Object.keys(topic)[0]}`);
-            topicTitleTag.textContent = Object.values(topic)[0] as string;
-            topicItemTag.appendChild(topicTitleTag);
-            element.appendChild(topicItemTag);
+        if (result.length) {
+            for (const topic of result) {
+                const topicItemTag = document.createElement('div');
+                topicItemTag.setAttribute('class', 'topic_item');
+                const topicTitleTag = document.createElement('a');
+                topicTitleTag.setAttribute('href', `detail.html?id=${Object.keys(topic)[0]}`);
+                topicTitleTag.textContent = Object.values(topic)[0] as string;
+                topicItemTag.appendChild(topicTitleTag);
+                element.appendChild(topicItemTag);
+            }
+        } else {
+            const messageTag = document.createElement('h2');
+            messageTag.textContent = "검색 결과를 찾을 수 없습니다";  
+            element.appendChild(messageTag); 
         }
     }
 }
